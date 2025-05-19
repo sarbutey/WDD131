56799baea2ce726fe7ac54eb7914778e12ee7330
sarbutey <sar24005@byui.edu>
2025-05-19 00:02:07 +0000

    temple

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+// temples.js
+
+// Set the current year in the footer
+const yearSpan = document.getElementById("year");
+if (yearSpan) {
+  const currentYear = new Date().getFullYear();
+  yearSpan.textContent = currentYear;
+}
+
+// Set the last modified date in the footer
+const modifiedSpan = document.getElementById("lastModified");
+if (modifiedSpan) {
+  modifiedSpan.textContent = document.lastModified;
+}
+
+
+
